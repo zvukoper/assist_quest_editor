5462e955ec4dc7c7e682921a714d81e5af7ca6ca
Mikhail Nikitin <soundgot@gmail.com>
2026-09-20 20:53:18 +0500

    test: исправить harness контракта выбора

--- ci/selection_context_smoke.mjs
+++ ci/selection_context_smoke.mjs
@@ -53,7 +53,7 @@ try {
           };
         </script>
         <script>
-          ${simulatorJs.replace(/<\\/script/gi, "<\\\\/script")}
+          ${simulatorJs.replaceAll("</script", "<\\/script")}
         </script>
       </body>
     </html>
