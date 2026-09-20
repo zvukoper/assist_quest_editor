bc1bbad82c698acbf76edcd6f19d767579bef1f1
Mikhail Nikitin <soundgot@gmail.com>
2026-09-20 21:07:15 +0500

    chore: синхронизировать идентификатор версии R1

--- src/AssistQuestEditor.App/Core/VersionInfo.cs
+++ src/AssistQuestEditor.App/Core/VersionInfo.cs
@@ -3,5 +3,5 @@ namespace AssistQuestEditor.App;
 public static class VersionInfo
 {
     public const string NumericVersion = "1.0.40.108";
-    public const string InformationalVersion = "1.0.40.108-QUEST-EDITOR-SDO-MAP-R6-SELECTION-CONTEXT-FIX";
+    public const string InformationalVersion = "1.0.40.108-QUEST-EDITOR-QUEST-GRAPH-R1";
 }
