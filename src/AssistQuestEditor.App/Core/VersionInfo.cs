ab967c17e5678cbbb8a2cf57ff7e20f0cfb5d755
Mikhail Nikitin <soundgot@gmail.com>
2026-09-20 18:36:48 +0500

    Зафиксировать рабочую версию интерфейса

---
+++ src/AssistQuestEditor.App/Core/VersionInfo.cs
@@ -0,0 +1,7 @@
+namespace AssistQuestEditor.App;
+
+public static class VersionInfo
+{
+    public const string NumericVersion = "1.0.40.101";
+    public const string InformationalVersion = "1.0.40.101-QUEST-EDITOR-DUAL-WINDOW-R1";
+}
