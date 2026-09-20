ccebb7c100acc6125fb2a805f676165fd6f09098
Mikhail Nikitin <soundgot@gmail.com>
2026-09-20 21:05:39 +0500

    chore: поднять версию для Quest Graph

--- src/AssistQuestEditor.App/Core/VersionInfo.cs
+++ src/AssistQuestEditor.App/Core/VersionInfo.cs
@@ -2,6 +2,6 @@ namespace AssistQuestEditor.App;
 
 public static class VersionInfo
 {
-    public const string NumericVersion = "1.0.40.107";
-    public const string InformationalVersion = "1.0.40.107-QUEST-EDITOR-SDO-MAP-R6-SELECTION-CONTEXT-FIX";
+    public const string NumericVersion = "1.0.40.108";
+    public const string InformationalVersion = "1.0.40.108-QUEST-EDITOR-SDO-MAP-R6-SELECTION-CONTEXT-FIX";
 }
