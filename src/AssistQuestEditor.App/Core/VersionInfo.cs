87443783aa3936b1b90d83a142df80544229afaf
Mikhail Nikitin <soundgot@gmail.com>
2026-09-20 21:54:45 +0500

    chore: поднять версию до 1.0.40.109

--- src/AssistQuestEditor.App/Core/VersionInfo.cs
+++ src/AssistQuestEditor.App/Core/VersionInfo.cs
@@ -2,6 +2,6 @@ namespace AssistQuestEditor.App;
 
 public static class VersionInfo
 {
-    public const string NumericVersion = "1.0.40.108";
-    public const string InformationalVersion = "1.0.40.108-QUEST-EDITOR-QUEST-GRAPH-R1";
+    public const string NumericVersion = "1.0.40.109";
+    public const string InformationalVersion = "1.0.40.109-QUEST-EDITOR-INTERACTION-R1";
 }
