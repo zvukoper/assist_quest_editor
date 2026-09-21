f6d05c93387501b8c527e1157cd9af272de94e25
Mikhail Nikitin <soundgot@gmail.com>
2026-09-21 21:33:16 +0500

    Поднять версию Scene Editor R2

--- src/AssistQuestEditor.App/Core/VersionInfo.cs
+++ src/AssistQuestEditor.App/Core/VersionInfo.cs
@@ -2,6 +2,6 @@ namespace AssistQuestEditor.App;
 
 public static class VersionInfo
 {
-    public const string NumericVersion = "1.0.40.131";
-    public const string InformationalVersion = "1.0.40.131-QUEST-SPLASH-R1";
+    public const string NumericVersion = "1.0.40.132";
+    public const string InformationalVersion = "1.0.40.132-QUEST-SPLASH-R1";
 }
