3ad308cade26b10eb6dd686415c17dbfd1485863
Mikhail Nikitin <soundgot@gmail.com>
2026-09-21 16:04:13 +0500

    Поднять версию для карты городов и hover

--- src/AssistQuestEditor.App/Core/VersionInfo.cs
+++ src/AssistQuestEditor.App/Core/VersionInfo.cs
@@ -2,6 +2,6 @@ namespace AssistQuestEditor.App;
 
 public static class VersionInfo
 {
-    public const string NumericVersion = "1.0.40.123";
-    public const string InformationalVersion = "1.0.40.123-QUEST-GRAPH-DRAG-CONNECT-R1";
+    public const string NumericVersion = "1.0.40.124";
+    public const string InformationalVersion = "1.0.40.124-QUEST-MAP-CITIES-HOVER-R1";
 }
