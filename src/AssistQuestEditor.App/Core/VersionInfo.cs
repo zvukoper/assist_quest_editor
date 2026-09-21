951bd59675d6884183bdfb40c8a1c826ad38769d
Mikhail Nikitin <soundgot@gmail.com>
2026-09-21 17:53:35 +0500

    chore: bump simulator inventory version

--- src/AssistQuestEditor.App/Core/VersionInfo.cs
+++ src/AssistQuestEditor.App/Core/VersionInfo.cs
@@ -2,6 +2,6 @@ namespace AssistQuestEditor.App;
 
 public static class VersionInfo
 {
-    public const string NumericVersion = "1.0.40.127";
-    public const string InformationalVersion = "1.0.40.128-QUEST-EXAMPLE-R1";
+    public const string NumericVersion = "1.0.40.129";
+    public const string InformationalVersion = "1.0.40.129-QUEST-SIM-INVENTORY-R1";
 }
