d85c52741d8df25be6060beab06ffd8dd72408fa
Mikhail Nikitin <soundgot@gmail.com>
2026-09-21 21:34:54 +0500

    Исправить informational version Scene Editor R2

--- src/AssistQuestEditor.App/Core/VersionInfo.cs
+++ src/AssistQuestEditor.App/Core/VersionInfo.cs
@@ -3,5 +3,5 @@ namespace AssistQuestEditor.App;
 public static class VersionInfo
 {
     public const string NumericVersion = "1.0.40.132";
-    public const string InformationalVersion = "1.0.40.132-QUEST-SPLASH-R1";
+    public const string InformationalVersion = "1.0.40.132-QUEST-SCENE-EDITOR-R2";
 }
