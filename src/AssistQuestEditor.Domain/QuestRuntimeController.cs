5256dce4523ca6e94163719a17a58531093a761f
Mikhail Nikitin <soundgot@gmail.com>
2026-09-22 15:41:32 +0500

    Расширить Runtime contract для симуляции и Quest activation

--- src/AssistQuestEditor.Domain/QuestRuntimeController.cs
+++ src/AssistQuestEditor.Domain/QuestRuntimeController.cs
@@ -4,10 +4,14 @@ public interface IQuestRuntimeController : IDisposable
 {
     QuestRuntimeState State { get; }
     QuestGraph? ActiveGraph { get; }
+    bool SimulationRunning { get; }
+    IReadOnlyCollection<string> EnabledQuestIds { get; }
     event EventHandler<QuestRuntimeEvent>? Published;
 
     void Start();
     void Stop(string reason = "Runtime остановлен");
     void Reset();
     void Tick();
+    void SetSimulationRunning(bool running);
+    void SetQuestEnabled(string questId, bool enabled);
 }
