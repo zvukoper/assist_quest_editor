ec4e87beb8bf26696fbaa1b7e4d4f6a607659068
Mikhail Nikitin <soundgot@gmail.com>
2026-09-22 09:48:30 +0500

    Version: bump Dialogue Workspace

--- src/AssistQuestEditor.App/Core/VersionInfo.cs
+++ src/AssistQuestEditor.App/Core/VersionInfo.cs
@@ -2,6 +2,6 @@ namespace AssistQuestEditor.App;
 
 public static class VersionInfo
 {
-    public const string NumericVersion = "1.0.40.136";
-    public const string InformationalVersion = "1.0.40.136-QUEST-DIALOGUE-AUTHORING-R1";
+    public const string NumericVersion = "1.0.40.137";
+    public const string InformationalVersion = "1.0.40.137-QUEST-DIALOGUE-WORKSPACE-R1";
 }
