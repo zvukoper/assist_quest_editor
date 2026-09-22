277f36b537bed0045e2c4f1f13ae31ff75a0139f
Mikhail Nikitin <soundgot@gmail.com>
2026-09-22 15:43:30 +0500

    Повысить версию Campaign Simulator

--- src/AssistQuestEditor.App/Core/VersionInfo.cs
+++ src/AssistQuestEditor.App/Core/VersionInfo.cs
@@ -2,6 +2,6 @@ namespace AssistQuestEditor.App;
 
 public static class VersionInfo
 {
-    public const string NumericVersion = "1.0.40.147";
-    public const string InformationalVersion = "1.0.40.147-QUEST-NAVIGATION-CONTEXT";
+    public const string NumericVersion = "1.0.40.148";
+    public const string InformationalVersion = "1.0.40.147-CAMPAIGN-SIMULATION";
 }
