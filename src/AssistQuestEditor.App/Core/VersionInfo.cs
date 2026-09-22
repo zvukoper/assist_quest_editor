329c0215aadafeeff1e8249b53c8ac6d4e05a5a7
Mikhail Nikitin <soundgot@gmail.com>
2026-09-22 08:10:28 +0500

    Поднять версию для Resource Formats R1

--- src/AssistQuestEditor.App/Core/VersionInfo.cs
+++ src/AssistQuestEditor.App/Core/VersionInfo.cs
@@ -2,6 +2,6 @@ namespace AssistQuestEditor.App;
 
 public static class VersionInfo
 {
-    public const string NumericVersion = "1.0.40.132";
-    public const string InformationalVersion = "1.0.40.132-QUEST-SCENE-EDITOR-R2";
+    public const string NumericVersion = "1.0.40.133";
+    public const string InformationalVersion = "1.0.40.133-RESOURCE-FORMATS-R1";
 }
