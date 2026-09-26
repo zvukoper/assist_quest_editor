ac7a5e98f649421b4c9c6abeb28deeba9cef85fc
Mikhail Nikitin <soundgot@gmail.com>
2026-09-26 17:22:21 +0500

    Синхронизировать VersionInfo с версией сборки

--- src/AssistQuestEditor.App/Core/VersionInfo.cs
+++ src/AssistQuestEditor.App/Core/VersionInfo.cs
@@ -2,6 +2,6 @@ namespace AssistQuestEditor.App;
 
 public static class VersionInfo
 {
-    public const string NumericVersion = "1.0.40.190";
-    public const string InformationalVersion = "1.0.40.190-SIMULATOR-CHILD-HOTKEYS-R1";
+    public const string NumericVersion = "1.0.40.191";
+    public const string InformationalVersion = "1.0.40.191-SIMULATOR-MAPVIEW-ROUTE-R1";
 }
